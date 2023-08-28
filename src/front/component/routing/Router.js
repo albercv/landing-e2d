7e16402f1b34c07a7fa004b8a04189e0d1896c02
Alberto Carrasco <al.carrasco.vazquez@gmail.com>
2023-08-28 11:44:35 +0200

    base and landing set up

---
+++ src/front/component/routing/Router.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Routes, Route, BrowserRouter } from "react-router-dom"
+import { Landing } from '../Landing'
+
+
+export const Router = () => {
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Landing />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    )
+}
