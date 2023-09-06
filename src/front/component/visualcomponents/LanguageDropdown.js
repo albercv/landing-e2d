67808e07b1dad5a1665dd71bffabd566a3a24280
Alberto Carrasco <al.carrasco.vazquez@gmail.com>
2023-09-06 12:56:58 +0200

    Spanish translation included. Language dropdown keeps language on change. Appointment button minimum size

--- src/front/component/visualcomponents/LanguageDropdown.js
+++ src/front/component/visualcomponents/LanguageDropdown.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import EN from '../../assets/images/eeuu.png'
 import ES from '../../assets/images/spain.png'
 import '../../css/visualcomponents/LanguageDropdown.css'
@@ -6,25 +6,42 @@ import { useTranslation } from 'react-i18next'
 
 export const LanguageDropdown = () => {
 
-    const {t, i18n} = useTranslation("global");
+    const { t, i18n } = useTranslation("global");
 
-    const [selectedOption, setSelectedOption] = useState('EN');
-    
+    const [selectedOption, setSelectedOption] = useState('');
+
+    const getStoragedLanguage = () => {
+        return localStorage.getItem('language');
+    }
+
+    useEffect(() => {
+        const storedLanguage = localStorage.getItem('language');
+        if (storedLanguage) {
+            return setSelectedOption(storedLanguage);
+        }
+        setSelectedOption("EN");
+    }, [])
 
     const handleOptionChange = (event) => {
-        setSelectedOption(event.target.value);
-        i18n.changeLanguage(event.target.value)
+        const language = event.target.value;
+        setSelectedOption(language);
+        storageLanguage(language);
+        i18n.changeLanguage(language);
     };
+
+
+
+    const storageLanguage = (language) => {
+        localStorage.setItem('language', language);
+    }
 
     return (
         <div className='dropdown-container'>
-            <img src={selectedOption === 'EN' ? EN : ES} className='dropdown-flag navbar-buttons-section' alt="flag" />    
-            <div>
-                <select className='dropdown-select' id="language" value={selectedOption} onChange={(event) => handleOptionChange(event)}>
-                    <option value="EN">{t("navbar.dropdown_english")}</option>
-                    <option value="ES">{t("navbar.dropdown_spanish")}</option>
-                </select>
-            </div>
+            <img src={selectedOption === 'EN' ? EN : ES} className='dropdown-flag navbar-buttons-section' alt="flag" />
+            <select className='dropdown-select' id="language" value={selectedOption} onChange={(event) => handleOptionChange(event)}>
+                <option className='dropdown-select-option' value="EN">{t("navbar.dropdown_english")}</option>
+                <option className='dropdown-select-option' value="ES">{t("navbar.dropdown_spanish")}</option>
+            </select>
         </div>
     )
 }
