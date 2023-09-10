0bdf79ce824febe0e0fc18b6a19f5c8adae4e7a7
Alberto Carrasco <al.carrasco.vazquez@gmail.com>
2023-09-10 12:30:42 +0200

    Adds text internationalization, fixes a small css issue with slider cards margin.

--- src/front/component/routing/Router.js
+++ src/front/component/routing/Router.js
@@ -1,17 +1,15 @@
 import React from 'react'
-import {Routes, Route, BrowserRouter } from "react-router-dom"
+import { Routes, Route, BrowserRouter } from "react-router-dom"
 import { Landing } from '../../Landing'
 
 
 
 export const Router = () => {
     return (
-        <div>
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/' element={<Landing />} />
-                </Routes>
-            </BrowserRouter>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Landing />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
